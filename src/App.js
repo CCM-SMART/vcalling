c183984a0f47e2aa21d9a4c0e2e11807f0998d59
Anthony Córdova <anthonycordova@gmail.com>
2021-07-03 12:23:52 -0500

    make it better

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react'
+import Video from './components/pages/Video';
+import Home from './components/pages/Home';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    render(){
+        return (
+            <>
+              <div>
+                <Router>
+                  <Switch>
+                    <Route path="/" exact component={Home} />
+                    <Route path="/:url" component={Video} />
+                  </Switch>
+                </Router>
+              </div>
+            </>
+        );
+    }
 }
-
 export default App;
